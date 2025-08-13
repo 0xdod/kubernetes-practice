1b7580543dd5e3c901c39161ebc3edf2dbcd97d9
Damilola Dolor <damiloladolor@gmail.com>
2025-08-13 11:18:05 +0100

    monitoring

---
+++ monitoring/app.js
@@ -0,0 +1,26 @@
+// index.js
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const counter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+});
+
+app.get('/', (req, res) => {
+  counter.inc();
+  res.send('Hello, world!');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
